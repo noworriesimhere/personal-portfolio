d3a8814590e79cba7134dc57bc01736c3be49b66
Alan Tran <contactalantran@gmail.com>
2020-11-16 23:56:43 -0500

    add intersection observer

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-
 navToggle.addEventListener("click", () => {
     document.body.classList.toggle('nav-open');
 })
@@ -11,3 +10,27 @@ for (let link of navLinks) {
         document.body.classList.remove('nav-open');
     })
 }
+
+// Navbar animation 
+
+class DesktopMenu {
+    constructor (menuItem, rootMargin, threshold) {
+        this.menuItem = document.getElementById(menuItem)
+        let observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    document.querySelector(`.${menuItem}`).classList.add("active")
+                } else {
+                    document.querySelector(`.${menuItem}`).classList.remove("active")
+                }
+            });
+            }, {rootMargin: `${rootMargin}px`, threshold: threshold});
+
+        observer.observe(this.menuItem);
+    }
+}
+
+new DesktopMenu("home", 5, .75) //parameters: section, rootMargin, threshold
+new DesktopMenu("myServices", 5, .8)
+new DesktopMenu("about", 5, .8)
+new DesktopMenu("work", 5, .75)
