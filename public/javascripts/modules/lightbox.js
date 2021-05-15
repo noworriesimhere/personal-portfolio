dfe04a315b750d7d20cb85fc0b1aa0d2c7a82f52
Alan Tran <contactalantran@gmail.com>
2021-05-15 16:42:32 -0400

    fixed modal left right buttons

--- public/javascripts/modules/lightbox.js
+++ public/javascripts/modules/lightbox.js
@@ -15,64 +15,81 @@ class Lightbox {
     for (let item of this.portfolioItems) {
 
       item.addEventListener('click', (e) => {
-        this.openThatTrash(e, item)
+        this.openThatTrash(item)
       });
     }
   }
 
-  openThatTrash(e, item) {
-    const previous = e.target.parentElement.previousElementSibling;
-    const next = e.target.parentElement.nextElementSibling;
+  openThatTrash(item) {
+    const previous = item.previousElementSibling;
+    const next = item.nextElementSibling;
     document.querySelector('body').style.overflow = 'hidden';
     this.lightbox.insertAdjacentHTML('afterbegin', `${lightboxItems[item.id]}`);
     this.lightbox.style.opacity = 1;
     this.lightbox.classList.add('lightbox__active');
     
+    setTimeout(() => document.querySelector('.lightbox__content').style.opacity = 1, 150)
+
     this.leftButton = document.querySelector('.lightbox__left');
-    console.log(this.leftButton)
+    if (!previous) {
+      this.leftButton.style.display = 'none'
+    }
+
     this.leftButton.addEventListener('click', (e) => { 
       this.closeThat();
       setTimeout(() => {
-        this.openThatTrash(e, previous);
-      }, 300)
+        this.openThatTrash(previous);
+      }, 250)
     })
     
   
     this.rightButton = document.querySelector('.lightbox__right');
+    if (!next) {
+      this.rightButton.style.display = 'none'
+    }
       this.rightButton.addEventListener('click', (e) => {
       this.closeThat();
       setTimeout(() => {
-        this.openThatTrash(e, next);
-      }, 500)
+        this.openThatTrash(next);
+      }, 250)
     })
     
 
     if (!this.closeBtn) {
       this.closeBtn = document.querySelector('.lightbox__close-btn');
-      this.closeBtn.addEventListener('click', (e) => {
-        this.closeThat();
+      this.closeBtn.addEventListener('click', ({target}) => {
+        this.closeThat(target.className);
+
       });
     }
   }
 
   closeInBackground() {
-    this.lightbox.addEventListener('click', (e) => {
-      if (e.target.className === 'lightbox lightbox__active') {
-        this.closeThat();
+    this.lightbox.addEventListener('click', ({target}) => {
+      if (target.className === 'lightbox lightbox__active') {
+        this.closeThat(target.className);
     }})
   }
 
-  closeThat() {
-    this.lightbox.style.opacity = 0;
+  closeThat(target) {
+    if (target === 'far fa-window-close lightbox__close-btn fa-3x' || target === 'lightbox lightbox__active') {
+      this.lightbox.style.opacity = 0;
+      setTimeout(() => this.lightbox.classList.remove('lightbox__active'), 200);
+    } 
     document.querySelector('body').style.overflow = 'unset';
-    setTimeout(() => this.lightbox.classList.remove('lightbox__active'), 200);
+    setTimeout(
+      () => document.querySelector('.lightbox__content').style.opacity = 0,
+      60
+    );
     setTimeout(
       () => document.querySelector('.lightbox__content').remove(),
-      250
+      210
     );
+    this.closeBtn.replaceWith(this.closeBtn.cloneNode(true))
     this.closeBtn = undefined;
-    // this.leftButton.replaceWith(this.leftButton.cloneNode(true))
+    this.leftButton.replaceWith(this.leftButton.cloneNode(true))
     this.leftButton = undefined;
+    this.rightButton.replaceWith(this.rightButton.cloneNode(true))
     this.rightButton = undefined;
   }
   
